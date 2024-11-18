4d54da2e6b5822a4816942d4d8f90af230c99c0c
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2024-11-18 20:08:16 +0200

    Added product name to order product serializer

--- app/serializers/order_product_serializer.rb
+++ app/serializers/order_product_serializer.rb
@@ -2,4 +2,8 @@ class OrderProductSerializer
   include JSONAPI::Serializer
 
   attributes :product_id, :quantity, :include_iron
+
+  attribute :product_name do |object|
+    object.product.name
+  end  
 end
