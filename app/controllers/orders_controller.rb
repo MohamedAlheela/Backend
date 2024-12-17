6a69200986150c03eac2211c6bb778e05b9fc7f1
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2024-12-17 20:00:17 +0200

    make deliverer access his orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,12 @@
 class OrdersController < ApplicationController
-  before_action :authorize_customer_or_admin
+  before_action :authorize_customer_or_admin, only: %i[create]
   before_action :set_order, only: %i[show update destroy]
   before_action :authorize_order_access, only: %i[show update destroy]
   before_action :check_ability_to_change_order, only: %i[update destroy]
 
   # GET /orders
   def index
-    orders =  if @current_user.admin?
-                paginate(Order.all.includes(:order_products))
-              else
-                paginate(@current_user.orders.includes(:order_products))
-              end
-
-    render_response(model_name: 'Order', data: orders, message: I18n.t('orders.orders_fetched_successfully'))
+    render_response(model_name: 'Order', data: fetch_orders, message: I18n.t('orders.orders_fetched_successfully'))
   end
 
   # GET /orders/:id
@@ -27,7 +21,6 @@ class OrdersController < ApplicationController
       create_order_products
       render_response(model_name: 'Order', data: @order, message: I18n.t('orders.order_created_successfully'), status: :created)
     else
-      # handle_error(@order.errors, I18n.t('orders.failed_to_create_order'))
       render_response_helper(message: @order.errors, status: :unprocessable_entity)
     end
   end
@@ -52,51 +45,42 @@ class OrdersController < ApplicationController
 
   private
 
+  def fetch_orders
+    orders = if @current_user.admin?
+                Order
+              elsif @current_user.deliverer?
+                @current_user.deliveries
+              else
+                @current_user.orders
+              end
+    paginate(orders.includes(:order_products))
+  end
+
   def set_order
     @order = Order.includes(:order_products).find(params[:id])
   end
 
   def authorize_order_access
-    return if @current_user.admin?
-
-    if @order.customer_id != @current_user.id      
-      render_response(message: I18n.t('errors.unauthorized_order_access'), status: :forbidden)
-    end
+    return if @current_user.admin? || @order.customer_id == @current_user.id || @order.deliverer_id == @current_user.id
+    render_response(message: I18n.t('errors.unauthorized_order_access'), status: :forbidden)
   end
 
   def check_ability_to_change_order
-    return if @current_user.admin?
-
-    if @order.deliverer_id.present?
-      render_response(message: I18n.t('errors.unauthorized_order_access'), status: :forbidden)
-    end
+    return if @current_user.admin? || @order.deliverer_id.blank?
+    render_response(message: I18n.t('errors.unauthorized_order_access'), status: :forbidden)    
   end
 
   def order_params
     params.require(:order).permit(:latitude, :longitude, :address, :country_id, :total_price, :deliverer_id, :customer_id, :delivery_time, order_products_attributes: [:product_id, :quantity, :include_iron])
   end
 
-  # def create_order_products
-  #   # Assuming order_products are nested within the order parameters
-  #   return unless params[:order][:order_products_attributes].present?
-    
-  #   params[:order][:order_products_attributes].each do |order_product_params|
-  #     @order.order_products.create!(order_product_params.permit(:product_id, :quantity, :include_iron))
-  #   end
-  # end
-
   def create_order_products
     return unless params[:order][:order_products_attributes].present?
     
     params[:order][:order_products_attributes].each do |order_product_params|
-      existing_order_product = @order.order_products.find_by(product_id: order_product_params[:product_id])
-      if existing_order_product
-        # If the product already exists, update the quantity instead of creating a new record
-        existing_order_product.update(quantity: existing_order_product.quantity + order_product_params[:quantity].to_i)
-      else
-        # Create a new OrderProduct if it doesn't exist
-        @order.order_products.create!(order_product_params.permit(:product_id, :quantity, :include_iron))
-      end
+      order_product = @order.order_products.find_or_initialize_by(product_id: order_product_params[:product_id])      
+      order_product.assign_attributes(order_product_params.slice(:quantity, :include_iron).compact)
+      order_product.save!
     end
   end
   
